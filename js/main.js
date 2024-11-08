8db70fef9844c4602d5b62e4157de00e7a549bba
srkvlntn <srkvlntn@gmail.com>
2024-11-08 09:31:52 +0200

    slider dots styled

--- js/main.js
+++ js/main.js
@@ -1,37 +1,48 @@
-$(document).ready(function() {
-    $('.menu-opener').on('click', function(e) { // Наша бургер кнопка на которую мы будем нажимать
+$(document).ready(function () {
+    $('.menu-opener').on('click', function (e) { // Наша бургер кнопка на которую мы будем нажимать
         e.preventDefault();
         $('body').toggleClass('mobile-menu-active') // body это селектор на который будет довешиваться класс mobile-menu-active при нажатии на бургер кнопку
     })
 })
 
 
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.intro-slider').slick({
-
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 15000,
+        arrows: false,
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-
-                }
-            },
             {
                 breakpoint: 768,
                 settings: {
-
+                    dots: true,
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 15000,
+                    arrows: false,
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-
+                    dots: true,
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 15000,
+                    arrows: false,
                 }
             }
 
         ]
-});
+    });
 });
 
 
