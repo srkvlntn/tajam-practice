80a61c09f5e6381f2b552044acdeb7e32e8e3eeb
srkvlntn <srkvlntn@gmail.com>
2024-11-08 17:28:48 +0200

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    $('.menu-opener').on('click', function (e) { // Наша бургер кнопка на которую мы будем нажимать
+    $('.menu-opener').on('click', function (e) {
         e.preventDefault();
-        $('body').toggleClass('mobile-menu-active') // body это селектор на который будет довешиваться класс mobile-menu-active при нажатии на бургер кнопку
+        $('body').toggleClass('mobile-menu-active')
     })
 })
 
